bdb86bc2fe029ba31ca4ba6c18c06c1e165154a1
bogosla <jdestine68@gmail.com>
2022-08-17 23:17:41 -0700

    create parseObject ParsePost

--- app/src/main/java/com/bogosla/binsta/MainActivity.java
+++ app/src/main/java/com/bogosla/binsta/MainActivity.java
@@ -4,15 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.bogosla.binsta.databinding.ActivityMainBinding;
+import com.bogosla.binsta.models.ParsePost;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainAcitvity";
     ActivityMainBinding biding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        DataBindingUtil.setContentView(MainActivity.this, R.layout.activity_main);
+        biding = DataBindingUtil.setContentView(MainActivity.this, R.layout.activity_main);
+
+        ParseQuery<ParsePost> query = ParseQuery.getQuery(ParsePost.class);
+        query.findInBackground(new FindCallback<ParsePost>() {
+            @Override
+            public void done(List<ParsePost> posts, ParseException e) {
+                if (e != null) return;
+                for (ParsePost post : posts) {
+                    Log.i(TAG, post.getDescription());
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/bogosla/binsta/models/ParsePost.java
+++ app/src/main/java/com/bogosla/binsta/models/ParsePost.java
@@ -1,2 +1,35 @@
-package com.bogosla.binsta.models;public class ParsePost {
+package com.bogosla.binsta.models;
+
+import com.parse.ParseClassName;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+@ParseClassName("Post")
+public class ParsePost extends ParseObject {
+    public static final String DESCRIPTION_KEY = "description";
+    public static final String IMAGE_KEY = "image";
+    public static final String USER_KEY = "user";
+
+    public String getDescription() {
+        return getString(DESCRIPTION_KEY);
+    }
+    public void setDescription(String description) {
+        put(DESCRIPTION_KEY, description);
+    }
+
+    public ParseFile getImage() {
+        return getParseFile(IMAGE_KEY);
+    }
+    public void setImage(ParseFile image) {
+        put(IMAGE_KEY, image);
+    }
+
+    public ParseUser getUser() {
+        return getParseUser(USER_KEY);
+    }
+    public void setUser(ParseUser user) {
+        put(USER_KEY, user);
+    }
+
 }
